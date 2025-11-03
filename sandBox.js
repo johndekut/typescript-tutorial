c482557cc69478850b570552bfc9fc4e2445acc5
John Muchiri <muchirijohn0018@gmail.com>
2025-11-03 11:59:25 +0300

    objects and array in ts: allowable changes

--- sandBox.js
+++ sandBox.js
@@ -1,9 +1,31 @@
-var character = "jonte";
-var age = 30;
-var isBlack = false;
-//character = 45
-character = "muchiri";
-var circ = function (diameter) {
-    return diameter * Math.PI;
+var names = ["luigi", "john", "muchiri"];
+names.push("kamenya"),
+    // names.push(45)
+    console.log(names);
+var numbers = [10, 30, 45, 50];
+numbers.push(34);
+numbers[2] = 10;
+console.log(numbers);
+// numbers.push("helo")
+// numbers[1] = "king"
+var mixed = ["ken", 4, "kim", 8, 9, false];
+mixed.push("alex");
+mixed.push(34);
+mixed.push(true);
+mixed[2] = 4;
+console.log(mixed);
+//objects
+var student = {
+    name: "John",
+    age: 45,
+    sex: "male",
+    email: "tyresejones@gmail.com"
 };
-console.log(circ(34));
+student.age = 34;
+student.name = "kimani";
+student = {
+    name: "Fatuma",
+    sex: "female",
+    email: "muchirifatuma@gmail.com",
+    age: 43
+};
--- sandBox.ts
+++ sandBox.ts
@@ -1,13 +1,38 @@
-let character = "jonte";
-let age = 30;
+let names = ["luigi", "john", "muchiri"]
+names.push("kamenya"),
+// names.push(45)
+console.log(names)
 
-let isBlack= false;
+let numbers = [10, 30, 45, 50]
+numbers.push(34);
+numbers[2] = 10;
+console.log(numbers)
+// numbers.push("helo")
 
-//character = 45
-character = "muchiri"
+// numbers[1] = "king"
 
-const circ = (diameter: number) =>{
-  return diameter *Math.PI
+let mixed = ["ken", 4, "kim", 8, 9, false]
+mixed.push("alex")
+mixed.push(34)
+mixed.push(true)
+mixed[2] = 4
+console.log(mixed)
+
+
+
+//objects
+let student ={
+  name:"John",
+  age:45,
+  sex:"male",
+  email:"tyresejones@gmail.com"
 }
-
-console.log(circ(34))
+student.age = 34;
+student.name= "kimani"
+student= {
+  name:"Fatuma",
+  sex: "female",
+  email:"muchirifatuma@gmail.com",
+  age:43
+  
+}
