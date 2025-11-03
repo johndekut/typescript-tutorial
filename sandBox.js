7388122e5cabf6566af2180dda858ae430b7bdce
John Muchiri <muchirijohn0018@gmail.com>
2025-11-03 12:28:18 +0300

    explicit types, union types in variables and objects

--- sandBox.js
+++ sandBox.js
@@ -1,31 +1,43 @@
-var names = ["luigi", "john", "muchiri"];
-names.push("kamenya"),
-    // names.push(45)
-    console.log(names);
-var numbers = [10, 30, 45, 50];
-numbers.push(34);
-numbers[2] = 10;
-console.log(numbers);
-// numbers.push("helo")
-// numbers[1] = "king"
-var mixed = ["ken", 4, "kim", 8, 9, false];
-mixed.push("alex");
-mixed.push(34);
+//EXPLICIT TYPES
+//initialize a variable, dont give it a value but in the future it will just take strings
+var character;
+var age;
+var isLoggedIn;
+//age = "kimani"
+age = 34;
+isLoggedIn = true;
+//arrays
+//declare a variable, let it recieve strings and initialzie it as an empty array
+var ninjas = [];
+//ninjas = [10, 34, 35]
+ninjas = ["alex", "kim"];
+ninjas.push("mark");
+console.log(ninjas);
+//union types - take more than one data type
+var mixed = [];
+mixed.push("kenneth", 34);
+mixed.push(45);
 mixed.push(true);
-mixed[2] = 4;
 console.log(mixed);
+var uid;
+uid = 123;
+uid = "wjywcwdcweyd";
+console.log(uid);
 //objects
-var student = {
-    name: "John",
-    age: 45,
-    sex: "male",
-    email: "tyresejones@gmail.com"
+var comrade;
+comrade = {
+    name: "jones",
+    age: 34,
+    registrationDetails: {
+        school: "dedan kimathi university",
+        isStudent: true
+    }
 };
-student.age = 34;
-student.name = "kimani";
-student = {
-    name: "Fatuma",
-    sex: "female",
-    email: "muchirifatuma@gmail.com",
-    age: 43
+comrade = ["jim"];
+console.log(comrade);
+var comrade2;
+comrade2 = {
+    name: "james",
+    age: 34,
+    beltColor: "blue"
 };
--- sandBox.ts
+++ sandBox.ts
@@ -1,38 +1,54 @@
-let names = ["luigi", "john", "muchiri"]
-names.push("kamenya"),
-// names.push(45)
-console.log(names)
+//EXPLICIT TYPES
+//initialize a variable, dont give it a value but in the future it will just take strings
+let character :string;
+let age: number;
+let isLoggedIn: boolean;
 
-let numbers = [10, 30, 45, 50]
-numbers.push(34);
-numbers[2] = 10;
-console.log(numbers)
-// numbers.push("helo")
+//age = "kimani"
+age = 34;
+isLoggedIn= true;
 
-// numbers[1] = "king"
+//arrays
+//declare a variable, let it recieve strings and initialzie it as an empty array
+let ninjas: string[] = [];
+//ninjas = [10, 34, 35]
+ninjas = ["alex", "kim"]
+ninjas.push("mark")
+console.log(ninjas)
 
-let mixed = ["ken", 4, "kim", 8, 9, false]
-mixed.push("alex")
-mixed.push(34)
+
+//union types - take more than one data type
+let mixed :(string|number|boolean)[] = [];
+mixed.push("kenneth", 34)
+mixed.push(45)
 mixed.push(true)
-mixed[2] = 4
-console.log(mixed)
+console.log(mixed);
 
-
+let uid: string|number;
+uid = 123;
+uid= "wjywcwdcweyd"
+console.log(uid)
 
 //objects
-let student ={
-  name:"John",
-  age:45,
-  sex:"male",
-  email:"tyresejones@gmail.com"
+let comrade : object;
+comrade = {
+  name: "jones",
+  age:34,
+  registrationDetails:{
+    school: "dedan kimathi university",
+    isStudent: true
+  }
 }
-student.age = 34;
-student.name= "kimani"
-student= {
-  name:"Fatuma",
-  sex: "female",
-  email:"muchirifatuma@gmail.com",
-  age:43
-  
+comrade= ["jim"]
+console.log(comrade)
+
+let comrade2 : {
+  name:string,
+  age: number,
+  beltColor:string
+}
+comrade2 = {
+  name:"james",
+  age: 34,
+  beltColor: "blue"
 }
